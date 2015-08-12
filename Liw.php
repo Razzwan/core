414542f9e0aac22a5aba12076db9583fd66ca3ee
Razzwan <razvanlomov@gmail.com>
2015-08-12 11:26:13 +0300

    RSR4

--- Liw.php
+++ Liw.php
@@ -2,6 +2,28 @@
 namespace liw\core;
 
 /**
+ * Файл инициализации приложения.
+ */
+
+if(defined('DEVELOP') && DEVELOP === true){
+    /**
+     * @const timestamp , Константа начала выполнения скрипта
+     */
+    define("TIME", microtime(true));
+
+    /**
+     * В режиме отладки должны отображаться все ошибки
+     */
+    error_reporting (E_ALL);
+    ini_set('display_errors', 1);
+}
+
+/**
+ * @const string PATH     корень liw каталога
+ */
+define('LIW_CORE', dirname(__DIR__) . DIRECTORY_SEPARATOR );
+
+/**
  * Class Liw
  * @package liw\core
  * Статичный класс, в котором хранятся суперглобальные переменные (пока)
@@ -20,3 +42,10 @@ class Liw
     ];
 
 }
+
+require LIW_CORE . '/core/Psr4AutoloaderClass.php'; //Подключение файла автозагрузки
+
+/**
+ * Регистрация автозагрузчика
+ */
+(new Psr4AutoloaderClass)->register();
